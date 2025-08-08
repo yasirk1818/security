9a0416498f4ccd5bd698bebf756c97a9f6b578de
yasirk1818 <yasirk1818@gmail.com>
2025-08-08 09:06:41 +0100

    Create Parent.js

---
+++ models/Parent.js
@@ -0,0 +1,11 @@
+// models/Parent.js
+const mongoose = require('mongoose');
+
+const ParentSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Parent', ParentSchema);
